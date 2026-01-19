6d1521cc8b541b6d176cb871de97fa993c76852b
Abel Alejandro López Cabero <abel.lopez97@outlook.com>
2026-01-18 20:10:57 -0400

    refactor: actualizando variables de entorno

--- src/services/apiConfig.js
+++ src/services/apiConfig.js
@@ -1,9 +1,12 @@
 // Configuración simple de la API
 const API_CONFIG = {
   // URL base del backend - CAMBIAR AQUÍ PARA CAMBIAR EL SERVIDOR
-  BASE_URL: 'http://localhost:3001',//'https://157-245-1-74.sslip.io',
-  WS_URL: 'ws://localhost:3001',//'wss://157-245-1-74.sslip.io',
-  
+  /*  BASE_URL: "http://localhost:3001", 
+  WS_URL: "ws://localhost:3001",  */
+
+  BASE_URL: "http://3.80.105.200:3001",
+  WS_URL: "ws://3.80.105.200:3001",
+
   // Función helper para construir URLs completas
   getUrl: (endpoint) => `${API_CONFIG.BASE_URL}${endpoint}`,
 };
